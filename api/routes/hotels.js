7dfa6ec3d8bb21dd1fdd7701bb225371473e4330
saurabhk98 <kamblessaurabh@gmail.com>
2023-01-10 22:28:19 -0500

    First push changes

---
+++ api/routes/hotels.js
@@ -0,0 +1,18 @@
+import express from "express"
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js"
+
+const router = express.Router();
+
+//CREATE
+router.post("/", createHotel);
+//UPDATE
+router.put("/:id", updateHotel);
+//DELETE
+router.delete("/:id", deleteHotel);
+//GET
+router.get("/:id", getHotel);
+//GETALL
+router.get("/", getHotels);
+
+export default router
